b55ff8215c662c07d38bd15677fb629cd5a88a7e
Marcelo Nardone <marcelonardone@MacBook-de-Marcelo.local>
2018-10-20 12:03:02 -0300

    first commit

---
+++ DolarAr/DolarAr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DolarAr
+//
+//  Created by Marcelo Nardone on 17/10/2018.
+//  Copyright © 2018 Marcelo Nardone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
